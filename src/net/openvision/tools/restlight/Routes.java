877446637ea67d1c413af0cf8b394b558b19bfe9
Hannes Widmoser <widmoser@gmail.com>
2013-11-08 00:06:25 +0100

    added documentation

--- src/net/openvision/tools/restlight/Routes.java
+++ src/net/openvision/tools/restlight/Routes.java
@@ -4,8 +4,16 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 
+/**
+ * Represents a set of routes that are supported by a particular
+ * {@link RestServlet}. The routes are defined in a routes file, which is parsed
+ * by a {@link Parser} resulting in an instance of this class.
+ * 
+ * @author Hannes Widmoser
+ * 
+ */
 public interface Routes {
-	
+
 	public static String[] SupportedMethods = { "POST", "PUT", "PATCH", "GET", "DELETE", "OPTIONS", "TRACE" };
 
 	public List<Route> getRoutes();
